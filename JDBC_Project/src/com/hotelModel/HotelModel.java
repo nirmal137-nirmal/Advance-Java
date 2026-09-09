82086be66dc62e068c86248b47de04915ba803fe
nirmal137-nirmal <nirmalfayake1307@gmail.com>
2026-09-09 13:43:22 +0530

    Hotel Model

--- JDBC_Project/src/com/hotelModel/HotelModel.java
+++ JDBC_Project/src/com/hotelModel/HotelModel.java
@@ -2,43 +2,138 @@ package com.hotelModel;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import com.hotelBundle.JDBCDataSource;
 
 public class HotelModel {
-	
-	// ADD
-	public static void add (HotelBean bean) {
-		
-		Connection con = null; 
-		
+
+	// NextPk
+
+	public static void nextPk() {
+
+		Connection con = null;
+		int pk = 0;
+
 		try {
 			con = JDBCDataSource.getConnection();
-			
 			con.setAutoCommit(false);
-			
+
+			PreparedStatement pstmt = con.prepareStatement("Select max(id) from hotel");
+
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				pk = rs.getInt(1);
+			}
+			con.commit();
+
+			System.out.println("Next Primary Key Createdl..");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			JDBCDataSource.trnRollback(con);
+		} finally {
+			JDBCDataSource.closeConnection(con);
+		}
+	}
+
+	// ADD
+	public static void add(HotelBean bean) {
+
+		Connection con = null;
+
+		try {
+			con = JDBCDataSource.getConnection();
+
+			con.setAutoCommit(false);
+
 			PreparedStatement pstmt = con.prepareStatement("Insert into hotel values (?,?,?,?,?)");
-			
+
 			pstmt.setLong(1, bean.getHotelId());
 			pstmt.setString(2, bean.getHotelName());
 			pstmt.setString(3, bean.getLocation());
 			pstmt.setDouble(4, bean.getRating());
 			pstmt.setString(5, bean.getContactNo());
-			
+
 			int i = pstmt.executeUpdate();
-			
+
 			con.commit();
-			
+
 			System.out.println("Data Inserted Successfully .." + i + " Row Effected ");
-					
-					
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			JDBCDataSource.trnRollback(con);
-		}finally {
+		} finally {
 			JDBCDataSource.closeConnection(con);
 		}
-	
+
+	}
+
+	// update
+
+	public static void update(HotelBean bean) {
+
+		Connection con = null;
+
+		try {
+			con = JDBCDataSource.getConnection();
+
+			con.setAutoCommit(false);
+
+			PreparedStatement pstmt = con.prepareStatement(
+					"update hotel set hotelName = ?, location = ?, rating = ?, contactNo = ? where hotelId = ?");
+
+			pstmt.setString(1, bean.getHotelName());
+			pstmt.setString(2, bean.getLocation());
+			pstmt.setDouble(3, bean.getRating());
+			pstmt.setString(4, bean.getContactNo());
+			pstmt.setLong(5, bean.getHotelId());
+
+			int i = pstmt.executeUpdate();
+
+			con.commit();
+
+			System.out.println("Data Update Successfully... " + i + " Row Effected ");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			JDBCDataSource.trnRollback(con);
+		} finally {
+			JDBCDataSource.closeConnection(con);
+		}
+
+	}
+
+	// Delete
+
+	public static void delete(long hotelId) {
+
+		Connection con = null;
+
+		try {
+			con = JDBCDataSource.getConnection();
+
+			con.setAutoCommit(false);
+
+			PreparedStatement pstmt = con.prepareStatement("Delete from hotel where hotelId = ?");
+
+			pstmt.setLong(1, hotelId);
+
+			int i = pstmt.executeUpdate();
+
+			con.commit();
+
+			System.out.println("Data Delete Successfully....");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			JDBCDataSource.trnRollback(con);
+		} finally {
+			JDBCDataSource.closeConnection(con);
+		}
+
 	}
 
 }
--- JDBC_Project/src/com/hotelModel/TestHotel.java
+++ JDBC_Project/src/com/hotelModel/TestHotel.java
@@ -1,25 +1,58 @@
 package com.hotelModel;
 
+import com.usermodel.UserModel;
+
 public class TestHotel {
-	
-	public static void main(String[] args) {
-		
-		testAdd();
+
+	public static void main(String[] args) throws Exception {
+
+//		testNextPk();
+//		testAdd();
+//		testUpdate();
+		testDelete();
 	}
-	
+
+	public static void testNextPk() throws Exception {
+
+		UserModel model = new UserModel();
+
+		model.nextPk();
+	}
+
 	public static void testAdd() {
-		
+
 		HotelModel model = new HotelModel();
 		HotelBean bean = new HotelBean();
-		
-		bean.setHotelId(1);
-		bean.setHotelName("Hotel Sayaji ");
-		bean.setLocation("Indore");
-		bean.setRating(5);
-		bean.setContactNo("7418529630");
-		
+
+		bean.setHotelId(6);
+		bean.setHotelName("Park Plaza");
+		bean.setLocation("Lucknow");
+		bean.setRating(6);
+		bean.setContactNo("7418596320");
+
 		model.add(bean);
-		
+
+	}
+
+	public static void testUpdate() {
+		HotelModel model = new HotelModel();
+		HotelBean bean = new HotelBean();
+
+		bean.setHotelName("Hotel Taj ");
+		bean.setLocation("Mumbai");
+		bean.setRating(8);
+		bean.setContactNo("7418529630");
+		bean.setHotelId(3);
+
+		model.update(bean);
+	}
+
+	public static void testDelete() {
+
+		HotelModel model = new HotelModel();
+
+		model.delete(6);
+
 	}
 
 }
