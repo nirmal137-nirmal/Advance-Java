1ca6bc4cf7379ab63a39cfdc7230cba1ac65c1bf
nirmal137-nirmal <nirmalfayake1307@gmail.com>
2026-09-10 13:34:28 +0530

    Event Model

--- JDBC_Project/src/com/eventmodel/EventModel.java
+++ JDBC_Project/src/com/eventmodel/EventModel.java
@@ -3,7 +3,10 @@ package com.eventmodel;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.doctorModel.DoctorBean;
 import com.eventBundle.JDBCDataSource;
 
 public class EventModel {
@@ -108,5 +111,119 @@ public class EventModel {
 			JDBCDataSource.closeConnection(con);
 		}
 
+	}
+
+	// Delete
+
+	public static void delete(long eventId) {
+
+		Connection con = null;
+
+		try {
+			con = JDBCDataSource.getConnection();
+
+			con.setAutoCommit(false);
+
+			PreparedStatement pstmt = con.prepareStatement("Delete from Event Where eventid = ?");
+
+			pstmt.setLong(1, eventId);
+
+			int i = pstmt.executeUpdate();
+
+			con.commit();
+
+			System.out.println("Data Deleted Successfully..." + i + " Row Effected");
+
+		} catch (Exception e) {
+			JDBCDataSource.turnRollBack(con);
+			e.printStackTrace();
+		} finally {
+			JDBCDataSource.closeConnection(con);
+		}
+
+	}
+
+	public List search(EventBean bean, int pageNo, int pageSize) throws Exception {
+
+		StringBuffer sql = new StringBuffer("SELECT * FROM event WHERE 1=1");
+
+		List<EventBean> list = new ArrayList<EventBean>();
+
+		Connection con = null;
+
+		try {
+
+			if (bean != null) {
+
+				// Event Id
+				if (bean.getEventId() > 0) {
+					sql.append(" AND eventId = " + bean.getEventId());
+				}
+
+				// Event Name
+				if (bean.getEventName() != null && bean.getEventName().length() > 0) {
+
+					sql.append(" AND eventName LIKE '" + bean.getEventName() + "%'");
+				}
+
+				// Event Date
+				if (bean.getEventdate() != null) {
+					sql.append(" AND eventDate = '" + bean.getEventdate() + "'");
+				}
+				
+				// Venue
+				if (bean.getVenue() != null && bean.getVenue().length() > 0) {
+
+					sql.append(" AND venue LIKE '" + bean.getVenue() + "%'");
+				}
+
+				// Organizer
+				if (bean.getOrganizer() != null && bean.getOrganizer().length() > 0) {
+
+					sql.append(" AND organizer LIKE '" + bean.getOrganizer() + "%'");
+				}
+			}
+
+			// Pagination
+			if (pageSize > 0) {
+
+				int index = (pageNo - 1) * pageSize;
+
+				sql.append(" LIMIT " + index + ", " + pageSize);
+			}
+
+			System.out.println("sql ====> " + sql.toString());
+
+			con = JDBCDataSource.getConnection();
+
+			PreparedStatement pstmt = con.prepareStatement(sql.toString());
+
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+
+				bean = new EventBean();
+
+				bean.setEventId(rs.getLong("eventId"));
+				bean.setEventName(rs.getString("eventName"));
+				bean.setEventdate(rs.getDate("eventDate"));
+				bean.setVenue(rs.getString("venue"));
+				bean.setOrganizer(rs.getString("organizer"));
+
+				list.add(bean);
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			if (con != null) {
+				con.close();
+			}
+		}
+
+		return list;
 	}
 }
--- JDBC_Project/src/com/eventmodel/TestEvent.java
+++ JDBC_Project/src/com/eventmodel/TestEvent.java
@@ -1,58 +1,90 @@
 package com.eventmodel;
 
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Iterator;
+import java.util.List;
 
 public class TestEvent {
-	
+
 	public static void main(String[] args) throws Exception {
-		
+
 //		testNextPk();
 //		testAdd();
-		testUpdate();
-		
+//		testUpdate();
+//		testDelete();
+		testSearch();
+
 	}
-	
+
 	public static void testNextPk() {
-		
+
 		EventModel model = new EventModel();
-		
+
 		model.nextPk();
-		
+
 	}
-	
+
 	public static void testAdd() throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		
-		EventModel  model = new EventModel();
+
+		EventModel model = new EventModel();
 		EventBean bean = new EventBean();
-	
+
 		bean.setEventId(1);
 		bean.setEventName("Java Workshop");
 		bean.setEventdate(sdf.parse("2026-09-18"));
 		bean.setVenue("IIPS Auditorium");
 		bean.setOrganizer("Code Academy");
-		
+
 		model.add(bean);
-		
+
 	}
-	
+
 	public static void testUpdate() throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		
+
 		EventModel model = new EventModel();
 		EventBean bean = new EventBean();
-		
+
 		bean.setEventName("Music Festival");
 		bean.setEventdate(sdf.parse("2026-09-20"));
 		bean.setVenue("Lal Parade Ground");
 		bean.setOrganizer("Rudraveer sena");
 		bean.setEventId(3);
-		
+
 		model.update(bean);
+	}
+
+	public static void testDelete() {
+
+		EventModel model = new EventModel();
+
+		model.delete(10);
+	}
+
+	public static void testSearch() throws Exception {
+
+		EventModel model = new EventModel();
+		EventBean bean = new EventBean();
+
+		List list = model.search(bean, 1, 10);
+
+		Iterator it = list.iterator();
+
+		while (it.hasNext()) {
+
+			bean = (EventBean) it.next();
+			System.out.println("Event Id : " + bean.getEventId());
+			System.out.println("Event Name : " + bean.getEventName());
+			System.out.println("Event Date : " + bean.getEventdate());
+			System.out.println("Event Venue : " + bean.getVenue());
+			System.out.println("Event Organizer : " + bean.getOrganizer());
+
+			System.out.println("-----------------------");
+		}
 	}
 
 }
